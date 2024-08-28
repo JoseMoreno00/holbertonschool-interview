67fdb13253e85f9c296ea563db5f40cbaa9f21fb
JoseMoreno00 <accgitjosem@gmail.com>
2024-08-28 16:54:29 -0300

    More Algorithms

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
+
+def canUnlockAll(boxes):
+    keys = []
+    count = 0
+    many = len(boxes)
+    for box in boxes:
+        if count == 0:
+            for value in box:
+                keys.append(value)
+        else:
+            if count in keys:
+                for value in box:
+                    keys.append(value)
+            else:
+               temp = count
+               for value in boxes[temp]:
+                    if temp in keys:
+                        keys.append(value)
+                    if temp == many:
+                        return False
+                    temp += 1
+        count += 1
+    return True
